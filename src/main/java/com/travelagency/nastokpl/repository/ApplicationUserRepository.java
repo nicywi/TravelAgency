0645f0f857cf5b85fc66b043095192081bda2853
Nikola Cywinska <nicywi@gmail.com>
2024-02-12 19:51:57 +0100

    reorganization

--- src/main/java/com/travelagency/nastokpl/repository/ApplicationUserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.travelagency.nastokpl.repository;
-
-import com.travelagency.nastokpl.entity.ApplicationUserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationUserRepository extends JpaRepository<ApplicationUserEntity, Long> {
-}
