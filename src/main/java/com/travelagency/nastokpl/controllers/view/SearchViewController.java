734420cfe28c732160ecd7c4812aee5cb2d0bc98
Nikola Cywinska <nicywi@gmail.com>
2024-03-09 13:45:25 +0100

    search.html added with bootstrap,SearchViewController fixed,

--- src/main/java/com/travelagency/nastokpl/controllers/view/SearchViewController.java
+++ src/main/java/com/travelagency/nastokpl/controllers/view/SearchViewController.java
@@ -1,25 +1,16 @@
 package com.travelagency.nastokpl.controllers.view;
 
 import com.travelagency.nastokpl.models.CityEntity;
-import com.travelagency.nastokpl.models.PurchaseEntity;
 import com.travelagency.nastokpl.models.TripEntity;
 import com.travelagency.nastokpl.service.SearchService;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -49,15 +40,8 @@ public class SearchViewController {
                                        @RequestParam(required = false) CityEntity destinationCityId,
                                        Model model) {
 
-        System.out.println("Duration days: " + durationDays);
-        if(priceAdult == null){
-            System.out.println("Price adult is null");
-        }
-        System.out.println("hello from get");
         List<TripEntity> searchResults = searchService.findTripsByCriteria(availableAdultSeats, priceAdult, startDate, endDate,
                 durationDays, mealType, priceChild, promoted, availableChildSeats, departureCityId, destinationCityId);
-
-        System.out.println("get size" + searchResults.size());
 
         model.addAttribute("searchResults", searchResults);
         return "search";
