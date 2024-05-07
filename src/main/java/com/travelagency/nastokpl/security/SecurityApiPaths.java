3ef18c1fde69fa4e3e3a5b3c838c7e05149d52e8
Piotr Falkowski <piotrfalkowski721@gmail.com>
2024-05-07 21:44:37 +0200

    clean code for package security

--- src/main/java/com/travelagency/nastokpl/security/SecurityApiPaths.java
+++ src/main/java/com/travelagency/nastokpl/security/SecurityApiPaths.java
@@ -6,11 +6,11 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum SecurityApiPaths {
-	HOME_PAGE("/"),
-	SIGN_IN_PAGE("/sign-in"),
-	SIGN_IN_API("/api/sign-in"),
-	SIGN_OUT_API("/api/sign-out"),
-	REGISTER_PAGE("/register/**");
+    HOME_PAGE("/"),
+    SIGN_IN_PAGE("/sign-in"),
+    SIGN_IN_API("/api/sign-in"),
+    SIGN_OUT_API("/api/sign-out"),
+    REGISTER_PAGE("/register/**");
 
-	private final String endpoint;
+    private final String endpoint;
 }
